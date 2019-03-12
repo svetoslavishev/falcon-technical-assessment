99841f34b03b7c67c50bff6d0ab7ec5b533973a7
Svetoslav Ishev <svetoslavishev@abv.bg>
2019-03-12 13:04:19 +0200

    added basic spring structure

---
+++ rest-api/src/main/java/com/ishev/bookstorerestapi/BookstorerestapiApplication.java
@@ -0,0 +1,13 @@
+package com.ishev.bookstorerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorerestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstorerestapiApplication.class, args);
+    }
+
+}
