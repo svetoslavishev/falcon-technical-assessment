8d87b814e28f839211164df122ede41316382513
Svetoslav Ishev <svetoslavishev@abv.bg>
2019-03-16 12:54:29 +0200

    catalog of items - ready

--- rest-api/src/main/java/com/ishev/bookstorerestapi/controllers/ItemRestController.java
+++ rest-api/src/main/java/com/ishev/bookstorerestapi/controllers/ItemRestController.java
@@ -1,9 +1,13 @@
 package com.ishev.bookstorerestapi.controllers;
 
+import com.ishev.bookstorerestapi.models.Item;
 import com.ishev.bookstorerestapi.services.item.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/items")
@@ -13,5 +17,10 @@ public class ItemRestController {
     @Autowired
     public ItemRestController(ItemService itemService) {
         this.itemService = itemService;
+    }
+
+    @GetMapping
+    public List<Item> showCatalog() {
+        return itemService.getAllItems();
     }
 }
--- rest-api/src/main/java/com/ishev/bookstorerestapi/repositories/item/SqlItemRepository.java
+++ rest-api/src/main/java/com/ishev/bookstorerestapi/repositories/item/SqlItemRepository.java
@@ -1,6 +1,7 @@
 package com.ishev.bookstorerestapi.repositories.item;
 
 import com.ishev.bookstorerestapi.models.Item;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -19,7 +20,21 @@ public class SqlItemRepository implements ItemRepository {
 
     @Override
     public List<Item> getAllItems() {
-        return null;
+
+        List<Item> result;
+
+        try(
+                Session session = sessionFactory.openSession()
+        ) {
+            session.beginTransaction();
+            result = session.createQuery("from Item").list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException();
+        }
+
+        return result;
     }
 
     @Override
